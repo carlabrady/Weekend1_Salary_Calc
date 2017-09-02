6c21be39704f493aa1f260a9722fd618a06da240
Carla Brady <carlakaybrady@gmail.com>
2017-09-02 11:08:17 -0500

    html framework

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,21 @@ console.log('js test');
 $(document).ready(onReady);
 
 function onReady() {
+    console.log('JQ test');
+    $('#addInfoButton').on('click', infoSubmit);
+}
+
+function infoSubmit() {
+    console.log('submit button has been clicked');
+    new Employee ($('#firstIn').val(), $('#lastIn').val(), $('#empNumIn').val(), $('#jobTitleIn').val(), $('#salaryIn').val())
     
+}
+
+function Employee(firstIn, lastIn, empNumIn, jobTitleIn, salaryIn) {
+    this.first = firstIn;
+    this.last = lastIn;
+    this.empID = empNumIn;
+    this.title = jobTitleIn;
+    this.salary = salaryIn;
+
 }
